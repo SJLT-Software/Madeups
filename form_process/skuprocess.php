614c0e9cfd967149c75649f2145ce2449e26b8cb
Manav Sureshbabu <manav.sureshbabu2022@vitstudent.ac.in>
2025-01-08 13:11:04 -0500

    New_SKU_Add_Form

--- form_process/skuprocess.php
+++ form_process/skuprocess.php
@@ -14,11 +14,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         exit();
     } else {
         $name = $_POST['Name'];
-        // $construction = $_POST['construction'];
         $tc = $_POST['tc'];
+        $fabricContent = $_POST['FabricContent'];
+        $weaveDesign = $_POST['WeaveDesign'];
+        $finishedWarpCount = $_POST['Finished_WarpCount'];
+        $finishedWarpComposition = $_POST['Finished_WarpComposition'];
+        $finishedWeftCount = $_POST['Finished_WeftCount'];
+        $finishedWeftComposition = $_POST['Finished_WeftComposition'];
+        $finishedEPI = $_POST['Finished_EPI'];
+        $finishedPPI = $_POST['Finished_PPI'];
+        $finishedPly = $_POST['Finished_Ply'];
+        $greigeWarpCount = $_POST['Greige_WarpCount'];
+        $greigeWarpComposition = $_POST['Greige_WarpComposition'];
+        $greigeWeftCount = $_POST['Greige_WeftCount'];
+        $greigeWeftComposition = $_POST['Greige_WeftComposition'];
+        $greigeEPI = $_POST['Greige_EPI'];
+        $greigePPI = $_POST['Greige_PPI'];
+        $greigePly = $_POST['Greige_Ply'];
+        $gsm = $_POST['GSM'];
+        $color = $_POST['Color'];
+        $finishedWidth = $_POST['Finished_Width'];
+        $greigeWidth = $_POST['Greige_Width'];
 
-        $insertQuery = "INSERT INTO main (sku, name, threadcount) VALUES ('$sku', '$name', '$tc')";
-        mysqli_query($con, $insertQuery);
+        $insertQuery = "INSERT INTO main (sku, name, threadcount, fabriccontent, weavedesign, finished_warpcount, finished_warpcomposition, finished_weftcount, finished_weftcomposition, finished_epi, finished_ppi, finished_ply, greige_warpcount, greige_warpcomposition, greige_weftcount, greige_weftcomposition, greige_epi, greige_ppi, greige_ply, gsm, color, finished_width, greige_width) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = mysqli_prepare($con, $insertQuery);
+        mysqli_stmt_bind_param($stmt, 'sssssssssssssssssssssss', $sku, $name, $tc, $fabricContent, $weaveDesign, $finishedWarpCount, $finishedWarpComposition, $finishedWeftCount, $finishedWeftComposition, $finishedEPI, $finishedPPI, $finishedPly, $greigeWarpCount, $greigeWarpComposition, $greigeWeftCount, $greigeWeftComposition, $greigeEPI, $greigePPI, $greigePly, $gsm, $color, $finishedWidth, $greigeWidth);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
 
         $_SESSION['dashboarderror'] = "SKU added successfully!!";
         header("Location: superuser.php");
--- superuser_components/skuform.php
+++ superuser_components/skuform.php
@@ -5,10 +5,68 @@
             <input id="sku" name="sku" type="text" class="form-field skuField" >
             <label for="Name">Name:</label>
             <input id="Name" name="Name" type="text" class="form-field" required>
-
-    
             <label for="tc">Thread Count:</label>
             <input id="tc" name="tc" type="number" class="form-field" required>
+            
+            <label for="FabricContent">Fabric Content:</label>
+            <input id="FabricContent" name="FabricContent" type="text" class="form-field" required>
+
+            <label for="WeaveDesign">Weave Design:</label>
+            <input id="WeaveDesign" name="WeaveDesign" type="text" class="form-field" required>
+
+            <label for="Finished_WarpCount">Finished Warp Count:</label>
+            <input id="Finished_WarpCount" name="Finished_WarpCount" type="text" class="form-field" required>
+
+            <label for="Finished_WarpComposition">Finished Warp Composition:</label>
+            <input id="Finished_WarpComposition" name="Finished_WarpComposition" type="text" class="form-field" required>
+
+            <label for="Finished_WeftCount">Finished Weft Count:</label>
+            <input id="Finished_WeftCount" name="Finished_WeftCount" type="text" class="form-field" required>
+
+            <label for="Finished_WeftComposition">Finished Weft Composition:</label>
+            <input id="Finished_WeftComposition" name="Finished_WeftComposition" type="text" class="form-field" required>
+
+            <label for="Finished_EPI">Finished EPI:</label>
+            <input id="Finished_EPI" name="Finished_EPI" type="text" class="form-field" required>
+
+            <label for="Finished_PPI">Finished PPI:</label>
+            <input id="Finished_PPI" name="Finished_PPI" type="text" class="form-field" required>
+
+            <label for="Finished_Ply">Finished Ply:</label>
+            <input id="Finished_Ply" name="Finished_Ply" type="text" class="form-field" required>
+
+            <label for="Greige_WarpCount">Greige Warp Count:</label>
+            <input id="Greige_WarpCount" name="Greige_WarpCount" type="text" class="form-field" required>
+
+            <label for="Greige_WarpComposition">Greige Warp Composition:</label>
+            <input id="Greige_WarpComposition" name="Greige_WarpComposition" type="text" class="form-field" required>
+
+            <label for="Greige_WeftCount">Greige Weft Count:</label>
+            <input id="Greige_WeftCount" name="Greige_WeftCount" type="text" class="form-field" required>
+
+            <label for="Greige_WeftComposition">Greige Weft Composition:</label>
+            <input id="Greige_WeftComposition" name="Greige_WeftComposition" type="text" class="form-field" required>
+
+            <label for="Greige_EPI">Greige EPI:</label>
+            <input id="Greige_EPI" name="Greige_EPI" type="text" class="form-field" required>
+
+            <label for="Greige_PPI">Greige PPI:</label>
+            <input id="Greige_PPI" name="Greige_PPI" type="text" class="form-field" required>
+
+            <label for="Greige_Ply">Greige Ply:</label>
+            <input id="Greige_Ply" name="Greige_Ply" type="text" class="form-field" required>
+
+            <label for="GSM">GSM:</label>
+            <input id="GSM" name="GSM" type="text" class="form-field" required>
+
+            <label for="Color">Color:</label>
+            <input id="Color" name="Color" type="text" class="form-field" required>
+
+            <label for="Finished_Width">Finished Width:</label>
+            <input id="Finished_Width" name="Finished_Width" type="text" class="form-field" required>
+
+            <label for="Greige_Width">Greige Width:</label>
+            <input id="Greige_Width" name="Greige_Width" type="text" class="form-field" required>
 
             <input type="submit" value="Submit">
 
